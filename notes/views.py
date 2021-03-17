41d907e960458b225202805eb88492694a9a85de
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-17 16:06:52 -0300

    django

---
+++ notes/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Olá mundo! Este é o app notes de Tecnologias Web do Insper.")
