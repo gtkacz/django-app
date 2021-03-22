3b9215f0a24951e3c0528d1b717e5ca11ae0eb42
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-21 21:07:13 -0300

    sabadou

--- notes/models.py
+++ notes/models.py
@@ -2,3 +2,8 @@ from django.db import models
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
+    content = models.TextField()
+    
+    #@classmethod
+    def __str__(self):
+        return f"{self.id}.{self.title}"
