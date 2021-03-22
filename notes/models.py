3101f6af6bdc42c44195f0a3d904b84bbc916bb3
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-22 09:17:35 -0300

    finalizando handout django

--- notes/models.py
+++ notes/models.py
@@ -4,6 +4,5 @@ class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     
-    #@classmethod
     def __str__(self):
         return (f"{self.id}.{self.title}")
