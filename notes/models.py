0e523d55b4b3f16b0356299705ec85a345ee3ada
Tkacz <gabrielmt2@al.insper.edu.br>
2021-03-23 11:07:48 -0300

    css add

--- notes/models.py
+++ notes/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 class Note(models.Model):
     title = models.CharField(max_length=200)
-    content = models.TextField()
+    content = models.TextField(default="")
+    tag = models.CharField(max_length=100, default="")
     
     def __str__(self):
         return (f"{self.id}.{self.title}")
