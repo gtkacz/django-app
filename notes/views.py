166a85cd20fc7166f10cfdf244db635014d9db7f
Tkacz <gabrielmt2@al.insper.edu.br>
2021-04-12 11:29:10 -0300

    finalizando c+

--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,7 @@ def index(request):
         content=request.POST.get('detalhes')
         id=request.POST.get('id')
         edit=request.POST.get('edit_note_id')
-        if id==None or id=="None" or id=='':
+        if (id==None or id=="None" or id=='') and (edit!=None or edit!="None" or edit!=''):
             note=Note(title=title, content=content)
             note.save()
         else:
