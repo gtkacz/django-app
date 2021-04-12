6d1cdf27f9fa69769e01309371b3caab7c689b08
Tkacz <gabrielmt2@al.insper.edu.br>
2021-04-12 10:49:59 -0300

    teste

--- notes/views.py
+++ notes/views.py
@@ -6,17 +6,18 @@ def index(request):
         title=request.POST.get('titulo')
         content=request.POST.get('detalhes')
         id=request.POST.get('id')
-        if id=="":
+        if id==None or id=="None" or id=='':
             note=Note(title=title, content=content)
             note.save()
         else:
-            if title == None:
+            if not title:
                 Note.objects.filter(id=id).delete()
             else:
                 edit=Note.objects.get(id=id)
                 edit.title=title
                 edit.content=content
                 edit.save()
+                
         return redirect('index')
     else:
         all_notes=Note.objects.all()
